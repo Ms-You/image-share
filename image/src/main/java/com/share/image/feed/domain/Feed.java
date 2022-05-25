3f361cf40ae94e855834d6442b500a8a632f2577
Myeongsu Yu <msu122005@hs.ac.kr>
2022-05-26 00:13:03 +0900

    Refactor: Add related mappings for other entities in Feed entity

--- image/src/main/java/com/share/image/feed/domain/Feed.java
+++ image/src/main/java/com/share/image/feed/domain/Feed.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -15,31 +16,35 @@ import java.util.List;
 public class Feed {
 
     @Id @GeneratedValue
+    @Column(name = "feed_id")
     private Long id;
 
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
 
     @Column(nullable = false)
     private String uploadImage;
 
-    @ManyToOne
-    private User writer;
-
-    @ManyToOne
-    private Tag tag;
-
-    @OneToMany
-    private List<Like> likes;
-
-    @OneToMany
-    private List<Reply> replies;
-
     @CreatedDate
     private LocalDateTime createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL)
+    private List<Like> likes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL)
+    private List<Reply> replies = new ArrayList<>();
+
 
 
 }
