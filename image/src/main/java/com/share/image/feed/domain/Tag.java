9a870628d9c20f78c06f31d8e4bd2ea4b364c115
Myeongsu Yu <msu122005@hs.ac.kr>
2022-05-25 23:35:23 +0900

    Feat: Create a Tag Entity
    Write a related mapping later

--- image/src/main/java/com/share/image/feed/domain/Tag.java
+++ image/src/main/java/com/share/image/feed/domain/Tag.java
@@ -1,4 +1,27 @@
 package com.share.image.feed.domain;
 
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Tag {
+
+    @Id @Generated
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany
+    private List<Feed> feeds;
+
 }
