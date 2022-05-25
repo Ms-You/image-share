8ac569d5edccf5f7b2017d423c406bf5e4406a19
Myeongsu Yu <msu122005@hs.ac.kr>
2022-05-25 22:41:21 +0900

    Feat: Create a Profile Entity
    Write a related mapping later

--- image/src/main/java/com/share/image/user/domain/Profile.java
+++ image/src/main/java/com/share/image/user/domain/Profile.java
@@ -1,4 +1,31 @@
 package com.share.image.user.domain;
 
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Profile {
+
+    @Id @Generated
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @Column(nullable = true)
+    private String profileImage;
+
+    @Column(nullable = true)
+    private String intro;
+
+
+
 }
