1dcedeed95fb6a28c7959c13033358ecd94386e5
Myeongsu Yu <msu122005@hs.ac.kr>
2022-05-25 23:38:23 +0900

    Feat: Create a Reply Entity
    Write a related mapping later

--- image/src/main/java/com/share/image/feed/domain/Reply.java
+++ image/src/main/java/com/share/image/feed/domain/Reply.java
@@ -1,4 +1,31 @@
 package com.share.image.feed.domain;
 
+import com.share.image.user.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Reply {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    private User writer;
+
+    @ManyToOne
+    private Feed feed;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
 }
