9a85ff330546baf2bafcd250a392929e54619424
Myeongsu Yu <msu122005@hs.ac.kr>
2022-05-26 00:06:10 +0900

    Refactor: Add related mappings for other entities in Profile entity

--- image/src/main/java/com/share/image/user/domain/Profile.java
+++ image/src/main/java/com/share/image/user/domain/Profile.java
@@ -11,15 +11,17 @@ import javax.persistence.*;
 public class Profile {
 
     @Id @GeneratedValue
+    @Column(name = "profile_id")
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @Column(nullable = true)
     private String profileImage;
 
-    @Column(nullable = true)
+    @Column(nullable = true, columnDefinition = "TEXT")
     private String intro;
 
 
