5de703b67014c9e6db8869ca06ce2ca0ae2476c3
Myeongsu Yu <msu122005@hs.ac.kr>
2022-05-25 23:34:10 +0900

    Feat: Create a Like Entity
    Write a related mapping later

--- image/src/main/java/com/share/image/feed/domain/Like.java
+++ image/src/main/java/com/share/image/feed/domain/Like.java
@@ -1,4 +1,25 @@
 package com.share.image.feed.domain;
 
+import com.share.image.user.domain.User;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Like {
+
+    @Id @Generated
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Feed feed;
 }
