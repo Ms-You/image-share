66a902e70aaa73dc47214e77535c1062b5a6f02b
Myeongsu Yu <msu122005@hs.ac.kr>
2022-05-29 23:08:23 +0900

    Feat: Create methods at UserRepository

--- image/src/main/java/com/share/image/user/repository/UserRepository.java
+++ image/src/main/java/com/share/image/user/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.share.image.user.repository;
 import com.share.image.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByNickName(String nickName);
+
+    Boolean existsByNickName(String nickName);
 }
