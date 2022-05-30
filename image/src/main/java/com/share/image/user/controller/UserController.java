48871b40f3263b62678c0f32c042bd9f1951758c
Myeongsu Yu <msu122005@hs.ac.kr>
2022-05-31 01:47:36 +0900

    Refactor: Use ResponseEntity in return format
    Use ResponseEntity to return message and HttpStatus code as json

--- image/src/main/java/com/share/image/user/controller/UserController.java
+++ image/src/main/java/com/share/image/user/controller/UserController.java
@@ -4,34 +4,33 @@ import com.share.image.user.domain.User;
 import com.share.image.user.dto.JoinRequestDto;
 import com.share.image.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class UserController {
 
     private final UserService userService;
 
-    // 회원가입 페이지 이동
-    @GetMapping("/user/join")
-    public String join(){
-        return "/user/join";
-    }
-
-    // 회원가입
+//     회원가입
+    @ResponseBody
     @PostMapping("/user/join")
-    public String join(JoinRequestDto joinRequestDto){
-        User joinedUser = userService.signUp(joinRequestDto);
-        return "redirect:/";
+    public ResponseEntity<?> join(@RequestBody JoinRequestDto joinRequestDto) {
+        try{
+            User joinedUser = userService.signUp(joinRequestDto);
+            return new ResponseEntity<>("회원가입이 완료되었습니다.", HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>("회원가입에 실패하였습니다.", HttpStatus.BAD_REQUEST);
+        }
     }
 
-    // 로그인 페이지 이동
-    @GetMapping("/user/login")
-    public String login(){
-        return "/user/login";
-    }
 
 
 }
