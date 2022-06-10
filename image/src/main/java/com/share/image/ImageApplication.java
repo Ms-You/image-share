1ee96f7f34564d0e5e8e89065722dfeebba06130
Myeongsu Yu <msu122005@hs.ac.kr>
2022-06-10 16:33:17 +0900

    Build: Modify PutMapping to available

--- image/src/main/java/com/share/image/ImageApplication.java
+++ image/src/main/java/com/share/image/ImageApplication.java
@@ -2,12 +2,21 @@ package com.share.image;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ImageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImageApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
 	}
 
 }
