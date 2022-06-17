9887a275d5bca257668bb3c615b7c916a6d50def
Myeongsu Yu <msu122005@hs.ac.kr>
2022-06-17 22:22:08 +0900

    Feat: Create a paging
    Create a paging when the user see feeds

--- image/src/main/java/com/share/image/feed/controller/TagController.java
+++ image/src/main/java/com/share/image/feed/controller/TagController.java
@@ -2,16 +2,21 @@ package com.share.image.feed.controller;
 
 import com.share.image.feed.domain.Feed;
 import com.share.image.feed.domain.Tag;
+import com.share.image.feed.repository.FeedRepository;
 import com.share.image.feed.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
+
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ import java.util.List;
 @RequestMapping("/user")
 public class TagController {
     private final TagRepository tagRepository;
+    private final FeedRepository feedRepository;
 
     // 로그인 후 태그 목록 보여줌
     @GetMapping
@@ -30,13 +36,15 @@ public class TagController {
     }
 
 
-    @GetMapping("/tag/{tag_id}")
-    public String tags(@PathVariable(name = "tag_id") Long tagId, Model model){
+    @GetMapping("/tag")
+    public String tags(@RequestParam(name = "tag_id") Long tagId, Model model, @PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
         Tag tag = tagRepository.findById(tagId).orElseThrow(()->{
             return new IllegalArgumentException("존재하지 않는 태그입니다.");
         });
 
-        List<Feed> feeds = tag.getFeeds();
+        Page<Feed> feeds = feedRepository.findByTag(tag, pageable);
+
+        model.addAttribute("tag", tag);
         model.addAttribute("feeds", feeds);
 
         return "tag/feeds";
--- image/src/main/java/com/share/image/feed/repository/FeedRepository.java
+++ image/src/main/java/com/share/image/feed/repository/FeedRepository.java
@@ -1,8 +1,11 @@
 package com.share.image.feed.repository;
 
 import com.share.image.feed.domain.Feed;
+import com.share.image.feed.domain.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
-
+    Page<Feed> findByTag(Tag tag, Pageable pageable);
 }
