cbb339ebb79993273d1a7c771e139cb7a0426999
Myeongsu Yu <msu122005@hs.ac.kr>
2022-06-24 00:19:42 +0900

    Feat: Create a view entity
    Create a view entity to prevent views from increasing when the same user views the same feed over and over again

---
+++ image/src/main/java/com/share/image/feed/domain/View.java
@@ -0,0 +1,37 @@
+package com.share.image.feed.domain;
+
+import com.share.image.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "views")
+public class View {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "view_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+
+    @Builder
+    public View(Long id, User user, Feed feed) {
+        this.id = id;
+        this.user = user;
+        this.feed = feed;
+    }
+
+
+}
