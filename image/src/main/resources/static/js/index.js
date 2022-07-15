5ae1c2b462fe37a07a01055fa3150389b1353f53
Myeongsu Yu <msu122005@hs.ac.kr>
2022-07-16 00:21:27 +0900

    Refactor: Redesign search button in navigation bar

--- image/src/main/resources/static/js/index.js
+++ image/src/main/resources/static/js/index.js
@@ -1,43 +1,58 @@
 // one page scroll
 window.onload = function(){
-      const elm = document.querySelectorAll('.move');
-      const elmCount = elm.length;
-      elm.forEach(function(item, index){
-        item.addEventListener('mousewheel', function(event){
-          event.preventDefault();
-          let delta = 0;
+  const elm = document.querySelectorAll('.move');
+  const elmCount = elm.length;
+  elm.forEach(function(item, index){
+    item.addEventListener('mousewheel', function(event){
+      event.preventDefault();
+      let delta = 0;
 
-          if (!event) event = window.event;
-          if (event.wheelDelta) {
-              delta = event.wheelDelta / 120;
-              if (window.opera) delta = -delta;
-          }
-          else if (event.detail)
-              delta = -event.detail / 3;
+      if (!event) event = window.event;
+      if (event.wheelDelta) {
+          delta = event.wheelDelta / 120;
+          if (window.opera) delta = -delta;
+      }
+      else if (event.detail)
+          delta = -event.detail / 3;
 
-          let moveTop = window.scrollY;
-          let elmSelector = elm[index];
+      let moveTop = window.scrollY;
+      let elmSelector = elm[index];
 
-          // wheel down : move to next section
-          if (delta < 0){
-            if (elmSelector !== elmCount-1){
-              try{
-                moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
-              }catch(e){}
-            }
-          }
+      // wheel down : move to next section
+      if (delta < 0){
+        if (elmSelector !== elmCount-1){
+          try{
+            moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
+          }catch(e){}
+        }
+      }
 
-          // wheel up : move to previous section
-          else{
-            if (elmSelector !== 0){
-              try{
-                moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
-              }catch(e){}
-            }
-          }
+      // wheel up : move to previous section
+      else{
+        if (elmSelector !== 0){
+          try{
+            moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
+          }catch(e){}
+        }
+      }
 
-          const body = document.querySelector('html');
-          window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
-        });
-      });
+      const body = document.querySelector('html');
+      window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+    });
+  });
+}
+
+
+function searchToggle(obj, evt){
+    var container = $(obj).closest('.search-wrapper');
+
+    if(!container.hasClass('active')){
+        container.addClass('active');
+        evt.preventDefault();
     }
+    else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+        container.removeClass('active');
+        // clear input
+        container.find('.search-input').val('');
+    }
+}
