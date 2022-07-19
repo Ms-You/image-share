1c482ca951b5036c8b1052679e0081f4d8bde0b5
Myeongsu Yu <msu122005@hs.ac.kr>
2022-07-19 17:49:12 +0900

    Feat: Create a report MVC
    Create a report MVC
    Create a report view page on the reports management of the admin page

---
+++ image/src/main/java/com/share/image/admin/controller/AdminReportController.java
@@ -0,0 +1,61 @@
+package com.share.image.admin.controller;
+
+import com.share.image.feed.domain.Report;
+import com.share.image.feed.repository.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/admin")
+public class AdminReportController {
+
+    private final ReportRepository reportRepository;
+
+    // 신고 목록 페이지 이동
+    @GetMapping("/report")
+    public String reportList(Model model, @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC)Pageable pageable){
+
+        Page<Report> reports = reportRepository.findAll(pageable);
+
+        int startPage = (int) (Math.floor(pageable.getPageNumber() / pageable.getPageSize()) * pageable.getPageSize() + 1);
+        int tempEndPage = startPage + pageable.getPageSize() - 1;
+        int endPage = tempEndPage > reports.getTotalPages() ? reports.getTotalPages() : tempEndPage;
+
+        model.addAttribute("reports", reports);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "admin/report/reportList";
+    }
+
+
+    // 특정 신고 보기
+    @GetMapping("/report/{report_id}")
+    public String reportView(@PathVariable(name = "report_id") Long reportId, Model model){
+
+        Report report = reportRepository.findById(reportId).orElseThrow(()->{
+            return new IllegalArgumentException("존재하지 않는 신고입니다.");
+        });
+
+        model.addAttribute("report", report);
+
+        return "admin/report/reportView";
+    }
+
+
+
+
+}
