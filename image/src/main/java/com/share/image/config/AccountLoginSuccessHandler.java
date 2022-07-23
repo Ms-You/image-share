a203dd5711d199cc7067bbfad9da028518d825e6
Myeongsu Yu <msu122005@hs.ac.kr>
2022-07-24 00:59:40 +0900

    Feat: Invoke an event that stores the ID and PW in a cookie
    Invoke an event that stores the ID and password in a cookie if checked in the check box "remember me" on the login page
    Invoke an event to clear cookie information if the check box "remember me" on the login page is unchecked

--- image/src/main/java/com/share/image/config/AccountLoginSuccessHandler.java
+++ image/src/main/java/com/share/image/config/AccountLoginSuccessHandler.java
@@ -1,6 +1,7 @@
 package com.share.image.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
@@ -13,10 +14,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @Transactional
@@ -32,6 +35,28 @@ public class AccountLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHa
 
         if (principal.getUser().getTemporaryLocked().equals("해제하기") || principal.getUser().getPermanentLocked().equals("해제하기")){
             throw new LockedException("계정이 정지되었습니다. 관리자에게 문의하세요.");
+        }
+
+
+        if (request.getSession().getAttribute("data") == null){
+            log.info("쿠키 만료");
+
+            Cookie userEmail = new Cookie("userEmail", null);
+            Cookie userPassword = new Cookie("userPassword", null);
+
+            userEmail.setMaxAge(0);
+            userPassword.setMaxAge(0);
+            response.addCookie(userEmail);
+            response.addCookie(userPassword);
+
+        } else if (request.getSession().getAttribute("data") != null){
+            log.info("쿠키 생성");
+
+            Cookie userEmail = new Cookie("userEmail", request.getParameter("email"));
+            Cookie userPassword = new Cookie("userPassword", request.getParameter("password"));
+
+            response.addCookie(userEmail);
+            response.addCookie(userPassword);
         }
 
         SavedRequest savedRequest = requestCache.getRequest(request, response);
