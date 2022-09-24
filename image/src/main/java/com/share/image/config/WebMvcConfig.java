f972bf87bffb02c2f153c327382fc8b411e41e33
Myeongsu Yu <msu122005@hs.ac.kr>
2022-09-25 00:35:50 +0900

    Refactor: Modify throwExceptionIfNoHandlerFound true
    Modify throwExceptionIfNoHandlerFound of DispatcherServlet to true to throw when the NoHandlerFoundException occurs
    Modify add-mapping to false to avoid mappings of ResourceHttpRequestHandler
    Add settings to WebMvcConfig to access static resources

--- image/src/main/java/com/share/image/config/WebMvcConfig.java
+++ image/src/main/java/com/share/image/config/WebMvcConfig.java
@@ -53,6 +53,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
 
-
+        /* '/js/**'로 호출하는 자원은 '/static/js/' 폴더 아래에서 찾는다. */
+        registry
+                .addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCachePeriod(60 * 60 * 24 * 365);
+        /* '/css/**'로 호출하는 자원은 '/static/css/' 폴더 아래에서 찾는다. */
+        registry
+                .addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(60 * 60 * 24 * 365);
+        /* '/img/**'로 호출하는 자원은 '/static/img/' 폴더 아래에서 찾는다. */
+        registry
+                .addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img/")
+                .setCachePeriod(60 * 60 * 24 * 365);
     }
 }
